6188bad83cab14d541f7d32d53a5a814c74be644
Deiner Zapata <deinerzapata@gmail.com>
2018-09-07 16:28:27 -0500

    Soport more 2 links

--- DownloadYouTube.py
+++ DownloadYouTube.py
@@ -27,10 +27,10 @@ def showDescriptionVideo(videoShow):
     print("Nick\t\t:"+videoShow.username)
     print("ID\t\t:"+videoShow.videoid)
 
-def downloadVideo(video2down,extension="mp4"):
+def downloadVideo(video2down):
     print("-----------------------------------------------------------------------------")
     print("Download video . . . [mp4]")
-    best = video2down.getbest(preftype=extension)
+    best = video2down.getbest(preftype="mp4")
     best.download()
 
 def downloadAudio(audio2down):
@@ -40,21 +40,38 @@ def downloadAudio(audio2down):
     best.download()
     
     AudioSegment.ffmpeg = "C:\\ffmpeg\\bin\\ffmpeg.exe"
-    sound = AudioSegment.from_file("C:/Users/LENOVO/Documents/PythonCode/YouTubeDownloader/audio.webm" ) #dir+"/YouTubeDownloader\\audio.webm")
-    sound.export("./audio.mp3",format="mp3",bitrate="128k")
+    nameAudioFile_webm = audio2down.title + ".webm"
+    nameAudioFile_mp3 = audio2down.title + ".mp3"
+    sound = AudioSegment.from_file(nameAudioFile_webm) #dir+"/YouTubeDownloader\\audio.webm")
+    sound.export(nameAudioFile_mp3,format="mp3",bitrate="128k")
+    os.remove(nameAudioFile_webm)
 
-def loadLinks2download(listLinks):
+def loadLinks2download():
     #Leer un archivo con los links txt de los videos o audios a descargar
-    print("-----------------------------------------------------------------------------")
-    print(str(listLinks))
-    
+    listLinks = open("links2download.txt","r")
+    links=[]
+    for linea in listLinks.readlines():
+        linea = linea[:-1]
+        if(linea=="audio" or linea=="video"):
+            formato = linea
+        else:
+            links.append(linea)
+    return formato,links
+
+def downloadListLinks(formato,listLinks):
+    for link in listLinks:
+        videoObj = pafy.new(link)
+        showDescriptionVideo(videoObj)
+        
+        if(formato == "video"):
+            downloadVideo(videoObj)
+        if(formato == "audio"):
+            downloadAudio(videoObj)
+            
 dir = os.getcwd() #path.abspath(__file__)
 print("-----------------------------------------------------------------------------")
 print("DownloadYouTube.py... ["+str(dir)+"]")
-link = "https://www.youtube.com/watch?v=5Y53boWWi8g"
-videoObj = pafy.new(link)
-showDescriptionVideo(videoObj)
-downloadVideo(videoObj)
-downloadAudio(videoObj)
+formato, listLinks = loadLinks2download()
+downloadListLinks(formato,listLinks)
 print("-----------------------------------------------------------------------------")
 
